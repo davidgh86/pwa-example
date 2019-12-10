b9db9af91b8816fa18c75d98e4561ef19c6c15d9
David <david_gh86@hotmail.es>
2019-12-10 21:31:20 +0100

    sw events

---
+++ sw.js
@@ -0,0 +1,16 @@
+// install service worker
+self.addEventListener('install', evt => {
+    console.log('service worker has been installed')
+});
+
+// activate service worker
+self.addEventListener('activate', evt => {
+    console.log('service workers has been activated')
+});
+
+// fetch event
+self.addEventListener('fetch', evt =>{
+    // console.log('fetch event', evt)
+    
+});
+
