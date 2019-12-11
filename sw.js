ab764ba57c42aa837a6252f899476fe4a401bc49
David <david_gh86@hotmail.es>
2019-12-11 21:10:31 +0100

    adding items to cache and improve caching strategy

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const staticCacheName = 'site-static';
+const staticCacheName = 'site-static-v2';
+const dynamicCacheName = 'site-dynamic-v1'
 const assets = [
     '/',
     '/index.html',
@@ -8,7 +9,9 @@ const assets = [
     '/css/styles.css',
     '/css/materialize.min.css',
     '/img/dish.png',
-    'https://fonts.googleapis.com/icon?family=Material+Icons'
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+    '/pages/fallback.html'
 ];
 
 // install service worker
@@ -24,12 +27,37 @@ self.addEventListener('install', evt => {
 
 // activate service worker
 self.addEventListener('activate', evt => {
-    console.log('service workers has been activated')
+    //console.log('service workers has been activated')
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            //console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+                .map(key => caches.delete(key))
+            )
+        })
+    )
 });
 
 // fetch event
 self.addEventListener('fetch', evt => {
     // console.log('fetch event', evt)
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            // catching from request if not from request
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCacheName).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            });
+        }).catch(() => {
+            // cuando la pagina no esta cacheada
+            if(evt.request.url.indexOf('.html') > -1) {
+                return caches.match('/pages/fallback.html')
+            }
+        })
+    )
 });
 
 
